ed371808145739bfc462028d95bc9a7fcb75e600
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-24 12:42:52 -0600

    added more test cases

--- test/Partnership.js
+++ test/Partnership.js
@@ -1,28 +1,39 @@
 const { expect } = require("chai");
 
+// Test suite for the Partnership contract
 describe("Partnership", () => {
-    it("can be deployed by providing two addresses on initalization", async () => {
-        const Contract = await hre.ethers.getContractFactory("Partnership");
+    it("can be deployed by providing at least two addresses and equal amounts of split ratios on initialization", async () => {
+        const Contract = await ethers.getContractFactory("Partnership");
 
-        const [owner, person1] = await hre.ethers.getSigners();
-
+        const [owner, person1] = await ethers.getSigners();
         const addresses = [owner.address, person1.address];
-        const contract = await Contract.deploy(addresses);
+
+        const splitRatios = [1, 1];
+
+        const contract = await Contract.deploy(addresses, splitRatios);
+
         await contract.deployed();
     });
-});
 
-it("can NOT be deployed when NOT providing addresses on initialization", async () =>
-{
-    const Contract = await hre.ethers.getContractFactory( "Partnership");
+    it("can not be deployed when the split ratios are not equivalent to the address amount", async () => {
+        const Contract = await ethers.getContractFactory("Partnership");
 
-    let error;
-    try {
-        const contract = await Contract.deploy();
-        await contract.deployed();
-    }catch (_error) {
-        error = _error;
-    } finally {
-        expect(error).to.be.ok;
-    }
+        const [owner, person1] = await ethers.getSigners();
+        const addresses = [owner.address, person1.address];
+
+        const splitRatios = [1, 1, 1];
+
+        await expect(Contract.deploy(addresses, splitRatios)).to.be.revertedWith("The address amount and the split ratio must be equal");
+    });
+
+    it("can not be deployed when the address amount is less than two", async () => {
+        const Contract = await ethers.getContractFactory("Partnership");
+
+        const [owner] = await ethers.getSigners();
+        const addresses = [owner.address];
+
+        const splitRatios = [1];
+
+        await expect(Contract.deploy(addresses, splitRatios)).to.be.revertedWith("The address amount must be at least two");
+    });
 });
