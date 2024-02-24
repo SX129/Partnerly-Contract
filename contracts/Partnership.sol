93728130ac184d1b93e50c8fcd00c9e2efee2426
Henry Nguyen <nguyenhenry682@gmail.com>
2024-02-24 02:33:59 -0600

    added state variables

--- contracts/Partnership.sol
+++ contracts/Partnership.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract Partnership {
-    constructor() {
-        console.log("Contract is deployed.");
+    string private deploymentMessage = "Contract is deployed.";
+    uint256 private partnerAmount = 2;
+    address[] public addresses;
+
+    constructor(address[] memory _addresses) {
+        require(
+            _addresses.length == partnerAmount,
+            "The can't have more than 2 partners."
+        );
+
+        addresses = _addresses;
+
+        console.log(deploymentMessage);
     }
 }
